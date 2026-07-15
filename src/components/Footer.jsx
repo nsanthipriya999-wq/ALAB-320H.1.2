c177cdbce5f3705205908d77b7ca10b2e28b57ac
santhi999 <nsanthipriya999@gmail.com>
2026-07-15 16:27:15 -0400

    react project   final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
-import Nav from "./Nav.jsx";
+import {footerlinks} from "./data.js";
 export default function Footer() {
     return (
         <footer>
-            <Nav />
+            <nav aria-label="Footer Navigation" role="navigation">
+                    <ul>
+                      {footerlinks.map((link)=>(
+            
+                      <li key={link}>
+            
+                        <a href="#">{link}</a>
+            
+                      </li>
+                      ))}
+                 </ul>
+                 </nav>
             <p>&copy; 2013 Valet Industries,Inc</p>
         </footer>
 
--- src/components/data.js
+++ src/components/data.js
@@ -1,1 +1,4 @@
 export const navlinks=["Women's", "Men's","On the Street", "The CatWalk", "AdWatch","About"];
+export const footerlinks=["Women's", "Men's","On the Street", "The CatWalk", "AdWatch","About","Tips"];
+
+
